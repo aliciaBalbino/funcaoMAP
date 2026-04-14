517131f65f46aaf726d52bf718580546cc4c7921
aliciaBalbino <alicia.balbino@outlook.com>
2026-04-14 19:07:27 -0300

    Add files via upload

---
+++ index (1).js
@@ -0,0 +1,25 @@
+let listaDePessoasAprovadas = [
+  
+  'ada.lovelace@example.com',
+  'marie.curie@example.com',
+  'margaret.hamilton@example.com',
+  'alan.turin@example.com'
+  
+  
+  ];
+  
+  const enviarEmail = (objetoEmail) => {
+    
+    console.log('Email para ${objetoEmail.email} com a nota ${objetoEmail.nota} enviado com sucesso!')
+    
+  };
+  
+  let listaDeEmails;
+  
+  listaDeEmails = listaDePessoasAprovadas.map((email) => {
+    
+    return {email: email, nota: 10};
+    
+  });
+  
+  console.log(listaDeEmails);
